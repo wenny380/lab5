b5b2138f1ce5a115e2ba02dea1800f131257656b
wenny380 <52930464+wenny380@users.noreply.github.com>
2021-05-29 02:56:58 +0300

    Add files via upload

---
+++ SoapFraemwork/SoapFraemwork/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoapFraemwork
+{
+    public partial class Form1 : Form
+    {
+        SoapClient client;
+        
+        public Form1()
+        {
+            client = new SoapClient();
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked)
+            {
+                textBox3.Text = CheckParse(textBox1.Text, textBox2.Text, client.Add);
+            }
+            else if (radioButton2.Checked)
+            {
+                textBox3.Text = CheckParse(textBox1.Text, textBox2.Text, client.Subtract);
+            }
+            else if (radioButton3.Checked)
+            {
+                textBox3.Text = CheckParse(textBox1.Text, textBox2.Text, client.Multiply);
+            }
+            else if (radioButton4.Checked)
+            {
+                textBox3.Text = CheckParse(textBox1.Text, textBox2.Text, client.Divide);
+            }
+            else
+            {
+                textBox3.Text = "Error";
+            }
+
+        }
+
+        private string CheckParse(string text1, string text2, Func<int, int, int> Meth)
+        {
+            int tmp1;
+            int tmp2;
+
+            if (int.TryParse(text1, out tmp1) && int.TryParse(text2, out tmp2))
+            {
+               return Meth(tmp1, tmp2).ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ SoapFraemwork/SoapFraemwork/SoapClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoapFraemwork
+{
+    class SoapClient
+    {
+        Calculator.CalculatorSoapClient mc = new Calculator.CalculatorSoapClient();
+
+
+        public int Add(int a, int b)
+        {
+            return mc.Add(a, b);
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return mc.Subtract(a, b);
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return mc.Multiply(a, b);
+        }
+
+        public int Divide(int a, int b)
+        {
+            if(b == 0)
+            {
+                return 0;
+            }
+            return mc.Divide(a, b);
+        }
+    }
+}
